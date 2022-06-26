c52ba382577d329af108be71d9b6aa8c0ec7b837
Severo-Yannick <severoyannick@gmail.com>
2022-06-26 16:52:32 +0200

    middleware for errors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require ('dotenv').config()
 const express = require('express')
+const errorMiddleware = require('./app/middlewares/error')
 const app = express()
 const api = require('./app/routes')
 
@@ -9,6 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', api)
+app.use(errorMiddleware)
 
 app.listen(PORT, err => {
     if (err) {
